d6a04d84f5dd3ad40bedfcbe15a44bb348fb3ca6
RustyRobot <rustfmtbot@yandex.ru>
2018-12-03 00:24:55 +0300

    rustyrobot formatting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use tokio::prelude::*;
 use actors::*;
+use tokio::prelude::*;
 
 #[macro_use]
 extern crate serde_derive;
@@ -15,14 +15,12 @@ impl Message for ValidMessageA {
     type Response = ValidMessageB;
 }
 
-impl Message for ValidMessageB { 
+impl Message for ValidMessageB {
     type Response = ();
 }
 
 impl Actor for TestActor {
-    fn on_spawn(&mut self, ctx: &mut Context<Self>)
-    {
-
+    fn on_spawn(&mut self, ctx: &mut Context<Self>) {
         let my_addr = ctx.addr();
         // my_addr.send(from(my_addr.recipient(), ValidMessageA));
         ctx.spawn(future::lazy(move || {
@@ -32,52 +30,44 @@ impl Actor for TestActor {
     }
 }
 
-impl Handler<SenderMessage<ValidMessageA>> for TestActor
-{
-    fn receive(&mut self, msg: SenderMessage<ValidMessageA>, ctx: &mut Context<Self>)
-    {
+impl Handler<SenderMessage<ValidMessageA>> for TestActor {
+    fn receive(&mut self, msg: SenderMessage<ValidMessageA>, ctx: &mut Context<Self>) {
         msg.sender().send(ValidMessageB(2));
         println!("handled A");
     }
 }
 
-impl Handler<ValidMessageB> for TestActor
-{
-    fn receive(&mut self, msg: ValidMessageB, ctx: &mut Context<Self>)
-    {
+impl Handler<ValidMessageB> for TestActor {
+    fn receive(&mut self, msg: ValidMessageB, ctx: &mut Context<Self>) {
         println!("handled B");
     }
 }
 
 fn main() {
     tokio::run(future::lazy(|| {
-        let addr1 = TestActor{ }.spawn();
-        let addr2 = TestActor{ }.spawn();
+        let addr1 = TestActor {}.spawn();
+        let addr2 = TestActor {}.spawn();
 
         let addr1listener = ListenerActor::<SenderMessage<ValidMessageA>>::new(
             "127.0.0.1:8080".to_string(),
             addr1.recipient(),
-        ).spawn();
-        let addr2listener = ListenerActor::<ValidMessageB>::new(
-            "127.0.0.1:8081".to_string(),
-            addr2.recipient(),
-        ).spawn();
+        )
+        .spawn();
+        let addr2listener =
+            ListenerActor::<ValidMessageB>::new("127.0.0.1:8081".to_string(), addr2.recipient())
+                .spawn();
 
-        let send1 = ConnectorActor::<ValidMessageB>::new(
-            "127.0.0.1:8080".to_string(),
-            addr2.recipient(),
-        ).spawn();
+        let send1 =
+            ConnectorActor::<ValidMessageB>::new("127.0.0.1:8080".to_string(), addr2.recipient())
+                .spawn();
 
-        let send2 = ConnectorActor::<ValidMessageB>::new(
-            "127.0.0.1:8081".to_string(),
-            addr1.recipient(),
-        ).spawn();
+        let send2 =
+            ConnectorActor::<ValidMessageB>::new("127.0.0.1:8081".to_string(), addr1.recipient())
+                .spawn();
 
         send2.send(remote(ValidMessageB(1)));
         send2.send(remote(ValidMessageB(1)));
         send2.send(remote(ValidMessageB(1)));
-        
-
 
         // let addr1proxyB = RemoteMessageActor::<ValidMessageB>::new(
         //     "127.0.0.1:8080".to_string(),
